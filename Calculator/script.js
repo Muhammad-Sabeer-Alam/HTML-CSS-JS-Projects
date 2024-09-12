92b6c1671b87094758f07eceba93ae162daee9d0
Muhammad-Sabeer-Alam <muhammadsabeeralam89@gmail.com>
2024-09-12 15:58:18 +0500

    Initial commit

---
+++ Calculator/script.js
@@ -0,0 +1,33 @@
+let string = "";
+let buttons = document.querySelectorAll(".btn");
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener("click", (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector("input").value = string;
+        }
+       else if(e.target.innerHTML == 'AC'){
+            string = "";
+            document.querySelector("input").value = string;
+        }
+       else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            document.querySelector("input").value = string;
+        }
+       else if(e.target.innerHTML == '%'){
+            try {
+                string = eval((string)/100).toString();
+                document.querySelector("input").value = string;
+            } catch {
+                document.querySelector("input").value = "Error";
+                string = "";
+            }
+        }
+        else{
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector("input").value = string;
+        }
+    })
+})
+
