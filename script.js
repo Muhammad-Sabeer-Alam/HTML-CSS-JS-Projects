1a320d90083588b22b6887136f2967c2fda4e2c7
Muhammad-Sabeer-Alam <muhammadsabeeralam89@gmail.com>
2024-10-26 08:59:25 +0500

    Ecommerce Product Page

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
-console.log("Welcome To Tic Tac Toe")
+// Initialize cart as an empty array
+let cart = []
 
-let music = new Audio("music.mp3");
-let audioTurn = new Audio("ting.mp3");
-let gameOver = new Audio("gameover.mp3");
-let turn = "X";
-let isgameover = false;
+// Buy Now button functionality
+document.querySelectorAll('.buy-now').forEach((button, index) => {
+    button.addEventListener('click', () => {
+        const productTitle = document.querySelectorAll('.product-title')[index].textContent;
+        alert(`You bought ${productTitle}`);
+    });
+});
 
-//Function To Change The Turn
-const changeTurn = () => {
-	return turn === "X" ? "0" : "X";
+// Add to Cart button functionality
+document.querySelectorAll('.add-to-cart').forEach((button, index) => {
+    button.addEventListener('click', () => {
+        const productTitle = document.querySelectorAll('.product-title')[index].textContent;
+        const productPrice = document.querySelectorAll('.product-price')[index].textContent;
+
+        // Add product to cart
+        cart.push({ title: productTitle, price: productPrice });
+        updateCartCount();
+
+        alert(`${productTitle} has been added to your cart.`);
+    });
+});
+
+// Update Cart Count Display
+function updateCartCount() {
+    const cartCount = document.querySelector('.cart-icon');
+    cartCount.textContent = cart.length;
 }
 
-//Function To Check For A Win
-const checkWin = () => {
-	let boxtext = document.getElementsByClassName('boxtext');
-	let wins = [
-		[0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 15, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135],
-	]
-	wins.forEach(e => {
-		if((boxtext[e[0]].innerText === boxtext[e[1]].innerText)&& (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== '')){
-			document.querySelector('.info').innerText = boxtext[e[0]].innerText + " WON";
-			isgameover = true;
-			document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-			document.querySelector('.line').style.width = "20vw";
-			document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
-		}
-	})
-}
+// Display Cart Items on Cart Icon Click
+document.querySelector('.cart-icon a').addEventListener('click', (e) => {
+    e.preventDefault();
+    if (cart.length === 0) {
+        alert("Your cart is empty!")
+    }
+    else {
+        let cartItems = "Items in your cart:\n";
+        cart.forEach((item, index) => {
+            cartItems += `${index + 1}. ${item.title} - ${item.price}\n`;
+        });
+        alert(cartItems);
+    }
+});
 
-//Game Logic
-let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element => {
-	let boxtext = element.querySelector('.boxtext');
-	element.addEventListener('click', () => {
-		if (boxtext.innerText === '') {
-			boxtext.innerText = turn;
-			turn = changeTurn();
-			audioTurn.play();
-			checkWin();
-			if(!isgameover){
-				document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
-			}
-		}
-	})
-})
+// Search Bar Functionality
+document.querySelector('.search-bar button').addEventListener('click', (e) => {
+    e.preventDefault();
+    const searchQuery = document.querySelector('.search-bar input[type="text"]').value.toLowerCase();
+    const products = document.querySelectorAll('.product-container');
 
-//Add onclick listener to reset button
-reset.addEventListener('click', () =>{
-	let boxtexts = document.querySelectorAll('.boxtext');
-	Array.from(boxtexts).forEach(element => {
-		element.innerText = '';
-	});
-	turn = "X";
-	isgameover = false;
-	document.querySelector('.line').style.width = "0vw";
-	document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
-	document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
-})
-
+    products.forEach(product => {
+        const productTitle = product.querySelector('.product-title').textContent.toLowerCase();
+        if (productTitle.includes(searchQuery)) {
+            product.style.display = "flex" // Show matching products 
+        }
+        else {
+            product.style.display = "none" // Hide non-matching products 
+        }
+    });
+});
 
